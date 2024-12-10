11a04e4ff6e7f61eadf303847e0e2e97c7dd85e2
Yao Ralph <yaoralph6@gmail.com>
2024-12-11 03:34:13 +0800

    branding changes

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,10 +48,10 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? 'Project By'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://github.com/yaoralph6'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
