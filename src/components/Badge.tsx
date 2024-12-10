ab5f73e07097d1a5a17f7d5cc3ea2618f318bf84
Yao Ralph <yaoralph6@gmail.com>
2024-12-11 03:41:08 +0800

    slight changes

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Yao, Ralph'}</span>
           </a>
         </span>
       </Show>
